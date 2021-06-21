468982b7c146842c3de097f48112b472443e2d52
isurusachitha <isbsachitha@gmail.com>
2021-06-21 19:12:38 +0530

    INIT:audio-upload-3

--- application/server/main.py
+++ application/server/main.py
@@ -52,7 +52,7 @@ def check_risk(symptom: Symptom):
 
 @app.post("/api/v1/predict/", tags=["Prediction"])
 async def predict(file: UploadFile = File(...)):
-    audio_data_in, sr_in = librosa.load(io.BytesIO(file))
+    audio_data_in, sr_in = librosa.load(file.file)
     length_in = len(audio_data_in) / sr_in
     #
     # predictor = PredictionService()
@@ -66,7 +66,7 @@ async def predict(file: UploadFile = File(...)):
 
 @app.post("/api/v2/predict/", tags=["Prediction"])
 async def predict(file: UploadFile = File(...)):
-    audio_data_in, sr_in = librosa.load(io.BytesIO(file.file))
+    audio_data_in, sr_in = librosa.load(file.file)
     length_in = len(audio_data_in) / sr_in
     #
     # predictor = PredictionService()
