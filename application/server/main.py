223a8e3f88e17a684d6fb4b3f4b70add8283e9a9
isurusachitha <isbsachitha@gmail.com>
2021-06-21 19:08:34 +0530

    INIT:audio-upload-2

--- application/server/main.py
+++ application/server/main.py
@@ -66,7 +66,7 @@ async def predict(file: UploadFile = File(...)):
 
 @app.post("/api/v2/predict/", tags=["Prediction"])
 async def predict(file: UploadFile = File(...)):
-    audio_data_in, sr_in = librosa.load(io.BytesIO(file))
+    audio_data_in, sr_in = librosa.load(io.BytesIO(file.file))
     length_in = len(audio_data_in) / sr_in
     #
     # predictor = PredictionService()
