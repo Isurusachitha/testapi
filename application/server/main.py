b59d21dfef27739dbd59cb4e47a83263d9a75656
isurusachitha <isbsachitha@gmail.com>
2021-06-23 21:49:09 +0530

    INIT:audio-upload-datav3-1

--- application/server/main.py
+++ application/server/main.py
@@ -39,31 +39,19 @@ async def index():
     return RedirectResponse(url="/docs")
 
 
+
 @app.post("/api/v1/predict/", tags=["Prediction"])
-async def predict(file: bytes = File(...)):
-    audio_data_in, sr_in = librosa.load(io.BytesIO(file))
-    length_in = len(audio_data_in) / sr_in
-
-    predictor = PredictionService()
-    diagnosis_predictions = predictor.get_prediction(audio_data_in, sr_in, length_in)
-
-    json_diagnosis_predictions = jsonable_encoder(list(diagnosis_predictions))
-
-    return {"predictions": 'json_diagnosis_predictions'}
+async def predict(audio: UploadFile = File(...)):
 
 
-@app.post("/api/v4/predict/", tags=["Prediction"])
-async def predict(image: UploadFile = File(...)):
-    print(image.file)
-    # print('../'+os.path.isdir(os.getcwd()+"images"),"*************")
     try:
         os.mkdir("images")
         print(os.getcwd())
     except Exception as e:
         print(e)
-    file_name = os.getcwd() + "/images/" + image.filename.replace(" ", "-")
+    file_name = os.getcwd() + "/audio/" + audio.filename.replace(" ", "-")
     with open(file_name, 'wb+') as f:
-        f.write(image.file.read())
+        f.write(audio.file.read())
         f.close()
 
     audio_data_in, sr_in = librosa.load(file_name)
@@ -77,46 +65,27 @@ async def predict(image: UploadFile = File(...)):
     return {"filename": json_diagnosis_predictions}
 
 
-@app.post("/api/v5/predict/", tags=["Prediction"])
-async def predict(file: bytes = File(...)):
-    audio_data_in, sr_in = librosa.load(file)
+
+@app.post("/api/v2/predict/", tags=["Prediction"])
+async def predict(audio: UploadFile = File(...)):
+
+    try:
+        os.mkdir("images")
+        print(os.getcwd())
+    except Exception as e:
+        print(e)
+    file_name = os.getcwd() + "/audio/" + audio.filename.replace(" ", "-")
+    with open(file_name, 'wb+') as f:
+        f.write(audio.file.read())
+        f.close()
+
+    audio_data_in, sr_in = librosa.load(file_name)
     length_in = len(audio_data_in) / sr_in
 
     predictor = PredictionService()
     diagnosis_predictions = predictor.get_prediction(audio_data_in, sr_in, length_in)
 
     json_diagnosis_predictions = jsonable_encoder(list(diagnosis_predictions))
-
-    return {"predictions": 'json_diagnosis_predictions'}
-
-
-@app.post("/api/v2/predict/", tags=["Prediction"])
-async def predict(file: UploadFile = File(...)):
-    content = await file.read()  # async read
-
-    audio_data_in, sr_in = librosa.load(file.file)
-    # length_in = len(audio_data_in) / sr_in
-    #
-    # predictor = PredictionService()
-    # diagnosis_predictions = predictor.get_prediction(audio_data_in, sr_in, length_in)
-    #
-    #
-    # json_diagnosis_predictions = jsonable_encoder(list(diagnosis_predictions))
-
-    return {"predictions": file.filename}
-
-
-@app.post("/api/v3/predict/", tags=["Prediction"])
-async def predict(file: UploadFile = File(...)):
-    print(type(file.file))
-    audio_data_in, sr_in = librosa.load(file.file)
-    length_in = len(audio_data_in) / sr_in
-    #
-    # predictor = PredictionService()
-    # diagnosis_predictions = predictor.get_prediction(audio_data_in, sr_in, length_in)
-    #
-    #
-    # json_diagnosis_predictions = jsonable_encoder(list(diagnosis_predictions))
 
     return {"predictions": "Healthy"}
 
